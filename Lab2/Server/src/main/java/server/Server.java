4224f4bb23b68ff0b0fc246eac920b1f696eae5b
edgaralves33017 <33017@alunos.isel.ipl.pt>
2020-11-17 20:23:53 +0000

    Lab02

--- Lab2/Server/src/main/java/server/Server.java
+++ Lab2/Server/src/main/java/server/Server.java
@@ -2,19 +2,20 @@ package server;
 
 import io.grpc.ServerBuilder;
 import io.grpc.stub.StreamObserver;
-import rpcstubs.Msg;
-import rpcstubs.Prime;
-import rpcstubs.SerieOfPrimes;
-import rpcstubs.ServiceAulaGrpc;
+import rpcstubs.*;
+import rpcstubs.Void;
 
 import java.util.Scanner;
+import java.util.concurrent.ConcurrentHashMap;
 
-public class Server  extends ServiceAulaGrpc.ServiceAulaImplBase {
-    static int svcPort=6000;
+public class Server extends ControlServiceGrpc.ControlServiceImplBase {
+    static int svcPort=7000;
+
+    //ID -- Entry Point
+    ConcurrentHashMap<String, CarInfo> cars = new ConcurrentHashMap<>();
+
     public static void main(String[] args) {
         try {
-            if (args.length > 0) svcPort=Integer.parseInt(args[0]);
-
             io.grpc.Server svc = ServerBuilder
                     .forPort(svcPort)
                     .addService(new Server())
@@ -28,44 +29,69 @@ public class Server  extends ServiceAulaGrpc.ServiceAulaImplBase {
         }
     }
 
-
-
     @Override
-    public void pingServer(Msg request, StreamObserver<Msg> responseObserver) {
-        Msg msgout=Msg.newBuilder().setTxt(request.getTxt()+" : server is alive").build();
-        responseObserver.onNext(msgout);
+    public void enter(Initial request, StreamObserver<Void> responseObserver) {
+        System.out.println("Car nº"+request.getId()+" entrou no ponto "+request.getInPoint());
+
+        cars.put(request.getId(), new CarInfo(request.getInPoint(), null));
+
+        responseObserver.onNext(Void.newBuilder().build());
         responseObserver.onCompleted();
     }
 
+    /*
+        O StreamObserver<WarnMsg> que vem por parametro é onde os warning messages são escritos para o cliente.
+        O StreamObserver<WarnMsg> que é retornado por este método é onde os clientes inserem os seus warnings.
+    */
     @Override
-    public void findPrimes(SerieOfPrimes request, StreamObserver<Prime> responseObserver) {
-        int contPrimes = 0;
-        int seqNum = request.getStartNumber(); // startNum > 1
-        if (seqNum % 2 ==0) seqNum++;
-        while (contPrimes < request.getNumberOfPrimes()) {
-            if (isPrime(seqNum)) {
-                responseObserver.onNext(
-                        Prime.newBuilder().setNumber(seqNum).build());
-                contPrimes++;
+    public StreamObserver<WarnMsg> warning (StreamObserver<WarnMsg> responseObserver) {
+        ServerStreamObserver reqs = new ServerStreamObserver(responseObserver);
+        return reqs;
+    }
+
+    @Override
+    public void leave(FinalPoint request, StreamObserver<Payment> responseObserver) {
+
+    }
+
+    private class ServerStreamObserver implements StreamObserver<WarnMsg> {
+
+        StreamObserver<WarnMsg> clientReplyStream;
+
+        public ServerStreamObserver(StreamObserver<WarnMsg> clientReplyStream) {
+            this.clientReplyStream = clientReplyStream;
+        }
+
+        @Override
+        public void onNext(WarnMsg warnMsg) {
+            if (warnMsg.getWarning().equals("")){
+                CarInfo info = cars.get(warnMsg.getId());
+                info.replyToClient = clientReplyStream;
+                cars.computeIfPresent(warnMsg.getId(), (key, oldValue) -> info);
             }
-            seqNum+=2;
+            else {
+                //Mandar para todos
+            }
         }
-        responseObserver.onCompleted();
 
-     }
+        @Override
+        public void onError(Throwable throwable) {
 
-    static boolean isPrime(int num) {
-        try {
-            Thread.sleep(1000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
         }
-        if (num <= 1) return false;
-        if (num == 2 || num == 3) return true;
-        if (num % 2 == 0) return false;
-        for (int i=3; i <= Math.sqrt(num); i+=2) {
-            if (num % i == 0) return false;
+
+        @Override
+        public void onCompleted() {
+            clientReplyStream.onCompleted();
         }
-        return true;
+    }
+
+    private class CarInfo {
+        Integer entrada;
+        StreamObserver<WarnMsg> replyToClient;
+
+        public CarInfo(Integer entrada, StreamObserver<WarnMsg> replyToClient) {
+            this.entrada = entrada;
+            this.replyToClient = replyToClient;
+        }
     }
 }
